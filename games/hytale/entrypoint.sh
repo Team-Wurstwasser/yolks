66f043892f49c53828f39373c8925374aef90016
hallo123wert <79359578+hallo123wert@users.noreply.github.com>
2026-01-13 18:48:05 +0100

    fix hytale (#4)

--- games/hytale/entrypoint.sh
+++ games/hytale/entrypoint.sh
@@ -5,7 +5,18 @@ cd /home/container
 
 # If HYTALE_SERVER_SESSION_TOKEN isn't set, assume the user will log in themselves, rather than a host's GSP
 if [[ -z "$HYTALE_SERVER_SESSION_TOKEN" ]]; then
-	./hytale-downloader/hytale-downloader-linux -patchline "$HYTALE_PATCHLINE"
+	./hytale-downloader/hytale-downloader-linux -patchline "$HYTALE_PATCHLINE" -download-path HytaleServer.zip
+
+	unzip -o HytaleServer.zip -d .
+
+	rm -f HytaleServer.zip
+elif [[ -f "HytaleServer.zip" ]]; then
+	unzip -o HytaleServer.zip -d .
+elif [[ -f "HytaleMount/HytaleServer.zip" ]]; then
+	unzip -o HytaleMount/HytaleServer.zip -d .
+else
+	echo -e "FATAL: HYTALE_SERVER_SESSION_TOKEN is set, but neither ./HytaleMount/HytaleServer.zip or ./HytaleServer.zip are present."
+	exit 1
 fi
 
 /java.sh $@
